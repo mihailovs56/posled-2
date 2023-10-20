91ee384b7770069e09c0cf1aceab2c72fbb02391
Михайлов <@DESKTOP-84VD5MI>
2023-10-20 20:59:53 +0500

    Добавьте файлы проекта.

---
+++ Практика 5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int a = 1;
+            while (a <= i)
+            {
+                Console.WriteLine("Последовательность чисел: " + a);
+                a *= 2;
+            }
+        }
+    }
+}
